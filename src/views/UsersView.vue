e2d690c261beb8a10cd9bea590feaeba673da761
Lymeng7031 <hort.lymeng@institute.pse.ngo>
2026-05-06 10:31:53 +0700

    add user-page

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,1 +1,18 @@
-<template><h1>Users Page</h1></template>
+<script setup>
+const userList = [
+  { id: 1, name: "Ly Meng", role: "Developer" },
+  { id: 2, name: "Bob", role: "Designer" },
+  { id: 3, name: "Charlie", role: "Manager" },
+];
+</script>
+
+<template>
+  <div>
+    <h1>Users Page</h1>
+    <ul>
+      <li v-for="user in userList" :key="user.id">
+        {{ user.name }} - {{ user.role }}
+      </li>
+    </ul>
+  </div>
+</template>
